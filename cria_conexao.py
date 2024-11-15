8131e8c0366daa163fee4c4fd9731a2439dd917d
Vinicius Saes <viniciusaesouza@gmail.com>
2024-11-15 15:38:57 -0300

    refactor: added .env layer

--- cria_conexao.py
+++ cria_conexao.py
@@ -1,7 +1,16 @@
 import oracledb
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+user = os.getenv("user")
+password = os.getenv("password")
+dsn = os.getenv("dsn")
+
 def recupera_conexao() -> oracledb.Connection:
+    
 	try:
-		conn = oracledb.connect(user="RM554456", password="080995", dsn="oracle.fiap.com.br:1521/ORCL")
+		conn = oracledb.connect(user=user, password=password, dsn=dsn)
 		return conn
 	except oracledb.OperationalError as e:
 		print("Falha ao criar conexão!")
