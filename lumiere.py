825e122bb0a2e0ab1524869e1cb836049dc2fd0f
Vinicius Saes <viniciusaesouza@gmail.com>
2024-11-17 15:05:00 -0300

    refactor login + venv update req.txt

--- lumiere.py
+++ lumiere.py
@@ -51,9 +51,9 @@ except Exception as e:
 # Recebe um texto:titulo como parâmetro e mostra no terminal
 def mostra_titulo(titulo:str):
     tam = len(titulo)
-    print('**' * (tam + 2))
+    print("**" * (tam + 2))
     print(f"{" " * (tam // 2)}--{titulo.upper()}--")
-    print('**' * (tam + 2))
+    print("**" * (tam + 2))
 
 
 def valida_formato(padrao:str, texto:str) -> bool:
@@ -62,17 +62,22 @@ def valida_formato(padrao:str, texto:str) -> bool:
     else:
         return False
     
-def valida_email_existente(email:str) -> list:
+def busca_email_existente(email:str) -> list:
     try:
         sql = f"SELECT * FROM tbl_usuarios_py WHERE email = :email"
         inst_sql.execute(sql, (email,))
         data = inst_sql.fetchall()
-        
         return data if data else []
     except Exception as e:
-        print(f"Falha ao validar o e-mail! ERRO: {e}")
+        print(f"Falha ao buscar e-mail na base de dados! ERRO: {e}")
         return []
         
+        
+        
+def cadastro():
+    pass
+
+
 def login():
     limpa_tela()
     login_info = {
@@ -83,30 +88,68 @@ def login():
     mostra_titulo("login")
     try:
         while True:
-            email = input("Email: ")
+            email = input("E-mail: ").lower()
             if valida_formato("email", email):
-                data = valida_email_existente(email)
+                data = busca_email_existente(email)
                 if data:
                     login_info['nome'] = data[0][1]
                     login_info['email'] = data[0][2]
                     login_info['senha'] = data[0][3]
-                    print(login_info)
+                    break
                 else:
-                    print("E-mail incorreto ou não cadastrado! Digite novamente.")
+                    print("---------------------")                
+                    print("E-mail incorreto ou ainda não cadastrado!")
+                    escolha = input("1. Digitar novamente \n2. Fazer Cadastro \nEscolha: ")
+                    match escolha:
+                        case "1":
+                            pass
+                        case "2":
+                            cadastro()
+                        case _:
+                            print("Opção inválida! Digite novamente: ")
+                            escolha = input("1. Digitar novamente \n2. Fazer Cadastro \nEscolha: ")
             else:
                 print("Formato do email inválido!")
+                
+        while True:    
+            senha = input("Senha: ")
+            if senha == login_info['senha']:
+                dashboard(login_info)
+                break
+            else:
+                print("---------------------")
+                print("Senha incorreta!")
+                while True:
+                    escolha = input("1. Digitar novamente \n2. Esqueci minha senha \nEscolha: ")
+                    match escolha:
+                        case "1":
+                            break
+                        case "2":
+                            editar_excluir_conta()
+                            break
+                        case _:
+                            print("---------------------")
+                            print("Opção inválida")
+            
     except Exception as e:
         print(f"ERRO: {e}")
 
+def editar_excluir_conta():
+    pass
+
+def dashboard(info:dict):
+    pass
+
 def main():
-    print(titulos['lumiere'])
-    print("""
+    limpa_tela()
+    mostra_titulo("lumiere")
+    escolha = input("""
 0. Sair
 1. Login
 2. Cadastro
 3. Editar | Excluir Conta
+Escolha: 
 """)
-    escolha = input("Escolha: ")
     match escolha:
         case "0":
             conn.close()
@@ -114,8 +157,9 @@ def main():
         case "1":
             login()
         case "2":
-            ...
+            pass
         case "3":
-            ...
+            pass
+
 if __name__ == '__main__':
     main()
